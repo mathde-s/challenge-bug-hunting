7a1374a4a7945b340a2dcee34a082e969cd46ef0
matheus <matheus.pereira@zup.com.br>
2024-12-29 21:24:25 -0300

    refactor: tirando a conversÃão de video e string da classe video

--- src/model/Video.java
+++ src/model/Video.java
@@ -38,19 +38,4 @@ public class Video {
         return dataPublicacao;
     }
 
-    @Override
-    public String toString() {
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-        return titulo + ";" + descricao + ";" + duracao + ";" + categoria + ";" + sdf.format(dataPublicacao);
-    }
-
-    public static Video fromString(String linha) {
-        try {
-            String[] partes = linha.split(";");
-            SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-            return new Video(partes[0], partes[1], Integer.parseInt(partes[2]), partes[3], sdf.parse(partes[4]));
-        } catch (Exception e) {
-            return null; // Ignora erros de parsing
-        }
-    }
 }
